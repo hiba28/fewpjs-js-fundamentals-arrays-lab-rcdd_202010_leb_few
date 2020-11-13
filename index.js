a1aae75ae0444c4110432781d059c83e9e2c3e0e
hiba abdel karim <hibaabdelkarim2000@gmail.com>
2020-11-13 23:28:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const prepend = ["Milo", "Otis", "Garfield"];
 const removeLast = ["Milo", "Otis", "Garfield"];
 const removeFirst = ["Milo", "Otis", "Garfield"];
 append.push("Odie");
-prepend.unshift("pet");
+prepend.unshift("Odie");
 removeLast.pop();
 removeFirst.shift();
